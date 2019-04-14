88ba471f63c427e9316c9caa50c945d51b427f17
gaurav1209 <aggarwal.gaurav10@gmail.com>
2019-04-14 21:58:10 +0530

    Shopping and Recipe components added

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,11 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Ingridient } from '../shared/ingridient.model';
+
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
+  ingridients: Ingridient[] = [
+    new Ingridient('A', 10),
+    new Ingridient('B', 3)
+  ];
 
   constructor() { }
 
