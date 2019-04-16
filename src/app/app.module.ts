c77d6faf7e886782e9d054a1d732aeb738c5d376
gaurav1209 <aggarwal.gaurav10@gmail.com>
2019-04-16 23:08:58 +0530

    Dropdown Added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-it
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
+import { DropdownDiective } from './shared/dropdown.directives';
 
 @NgModule({
   declarations: [
@@ -20,6 +21,7 @@ import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-ed
     RecipeDetailComponent,
     ShoppingListComponent,
     ShoppingEditComponent,
+    DropdownDiective
   ],
   imports: [
     BrowserModule
