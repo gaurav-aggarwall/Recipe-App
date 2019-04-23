f6e30f3cf577d44ef65442659e1c9e8ce0e889b7
gaurav1209 <aggarwal.gaurav10@gmail.com>
2019-04-23 15:47:09 +0530

    Recipe Service Updated

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,10 +1,14 @@
+import { EventEmitter } from '@angular/core';
+
 import  { Recipe } from './recipe.model';
 
 export class RecipeService {
     private recipes: Recipe[] = [
         new Recipe('Test Recipe', 'A test', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTdcYEKGIgkO7wnX_k1tbsAaYEiA6BvcD3zPAoBo8csaW8EAopR'),
         new Recipe('Test Recipe', 'A test', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTdcYEKGIgkO7wnX_k1tbsAaYEiA6BvcD3zPAoBo8csaW8EAopR')
-        ];
+    ];
+
+    recipeSelected = new EventEmitter<Recipe>();  
 
     getRecipes(){
         return this.recipes.slice();
